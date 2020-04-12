430bc98d0ee3fcfb9cae6b7e8ddcce1b8f489870
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-04-13 00:18:35 +0800

    Gatsby

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  apiKey: '#',
-  authDomain: '#',
-  databaseURL: '#',
-  projectId: '#',
-  storageBucket: '#',
-  messagingSenderId: '#',
-  appId: '#',
+  apiKey: "AIzaSyCDykT0vEO7X-62od7Bl5i7XZfP5pIlNoY",
+  authDomain: "gamify-github-1b1c5.firebaseapp.com",
+  databaseURL: "https://gamify-github-1b1c5.firebaseio.com",
+  projectId: "gamify-github-1b1c5",
+  storageBucket: "gamify-github-1b1c5.appspot.com",
+  messagingSenderId: "522492252077",
+  appId: "1:522492252077:web:ca31fe027c2f692b97db24",
+  measurementId: "G-9B1WZ1C87V"
 };
